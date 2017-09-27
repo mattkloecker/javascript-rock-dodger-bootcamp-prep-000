76ed59ddbfe485e90e4d54268e9414f45152a38d
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-27 17:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,6 +138,12 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
+  var leftNumbers=dodger.style.left.replace('px','')
+  var left = parseInt(leftNumbers,10)
+
+  if (left>0){
+    dodger.style.left=`${left-4}px`
+  }
   // implement me!
   /**
    * This function should move DODGER to the left
