e2ae14b0447485b133861811bfe1d3dddabaeeea
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-27 17:18:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ GAME.appendChild(rock)
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  
+
 
   // We should kick of the animation of the rock around here
 
